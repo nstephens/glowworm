7939ef701d5ced6df8e59fd66d3fd94d9a467bd7
nickstephens@gmail.com <nickstephens@gmail.com>
2026-03-03 11:59:33 -0500

    Add debug sprite info to renderer status

--- display/glowworm_display/renderer.py
+++ display/glowworm_display/renderer.py
@@ -1122,6 +1122,16 @@ class Renderer:
                 "transition_count": self.stats.transition_count,
                 "errors_count": self.stats.errors_count,
             },
+            # Debug: sprite info
+            "debug_sprites": {
+                "current_sprite": self._current_sprite is not None,
+                "next_sprite": self._next_sprite is not None,
+                "current_top_sprite": self._current_top_sprite is not None,
+                "current_bottom_sprite": self._current_bottom_sprite is not None,
+                "next_top_sprite": self._next_top_sprite is not None,
+                "next_bottom_sprite": self._next_bottom_sprite is not None,
+                "transition_running": self._transition is not None and self._transition.is_running if self._transition else False,
+            },
         }
 
         # Include error details if in error state
